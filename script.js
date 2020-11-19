a707901480db696a72c150b87e35628c9531393d
mauromaraujo <74024758+mauromaraujo@users.noreply.github.com>
2020-11-18 22:03:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let Inicio=document.getElementById("Inicio");
+let SobreMi=document.getElementById("SobreMi");
+let Inicio=document.getElementById("Contacto");
+Inicio.addEventListener('click', function(){
+    Inicio.scrollIntoView(true);
+});
+SobreMi.addEventListener('click', function(){
+    SobreMi.scrollIntoView(true);
+});
+Contacto.addEventListener('click', function(){
+    Contacto.scrollIntoView(true);
+});
